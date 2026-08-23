c832bba430c1899e113ec4c5fe10e4cba73f8775
s-cyber-project <sougatapaul4414@gmail.com>
2026-08-23 10:24:32 +0530

    Switch MySQL connection to use environment variables

--- mysql.js
+++ mysql.js
@@ -1,9 +1,10 @@
 const sql=require('mysql2');
 const db=sql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "3223",
-    database:"project"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3000
 });
 db.getConnection((err, connection)=>{
     if(err){
