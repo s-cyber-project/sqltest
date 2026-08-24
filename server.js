9561c6cf4ed79705d2c3acf9ef7cb132b8baf5c2
s-cyber-project <sougatapaul4414@gmail.com>
2026-08-24 13:07:02 +0530

    Load environment variables using dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+require('dotenv').config();
 const db = require('./mysql');
 
 const port = process.env.PORT || 3000;
