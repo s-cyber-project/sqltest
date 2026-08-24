85c5189933166b3a14ad553c8a33966881dfcd58
s-cyber-project <sougatapaul4414@gmail.com>
2026-08-24 12:54:11 +0530

    Refactor database connection configuration and query

--- mysql.js
+++ mysql.js
@@ -1,18 +1,19 @@
-const Pool = require('pg');
+const { Pool } = require('pg');
 
 const db = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-db.getConnection((err, connection)=>{
-    if(err){
+db.query('SELECT * FROM book')
+    .then(result => {
+        console.log('DATABASE CONNECTED');
+        console.log(result.rows);
+    })
+    .catch(err => {
+        console.log('DATABASE ERROR:');
         console.log(err);
-    }else{
-        console.log('Database connected');
-    }
-});
-module.exports=db;
-
+    });
+module.exports = db;
